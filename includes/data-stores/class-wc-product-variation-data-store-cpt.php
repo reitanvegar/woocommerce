50947ecc1c1d914327f11ac247c9809e7d1eb9d5
James Allan <james.allan@automattic.com>
2020-10-22 17:21:15 +1000

    Use strict comparison to compare post dates
    With PHP 8.0, non-strict comparisons between integers and strings containing
    non-numeric characters are being tightened. This affects comparisons like:
    
    0 < '0000-00-00 00:00:00'
    
    PHP 8.0 that equates to true whereas prior to 8.0 it would be false.
    
    More details of this change can be found at: https://wiki.php.net/rfc/string_to_number_comparison

--- includes/data-stores/class-wc-product-variation-data-store-cpt.php
+++ includes/data-stores/class-wc-product-variation-data-store-cpt.php
@@ -62,8 +62,8 @@ class WC_Product_Variation_Data_Store_CPT extends WC_Product_Data_Store_CPT impl
 			array(
 				'name'              => $post_object->post_title,
 				'slug'              => $post_object->post_name,
-				'date_created'      => 0 < $post_object->post_date_gmt ? wc_string_to_timestamp( $post_object->post_date_gmt ) : null,
-				'date_modified'     => 0 < $post_object->post_modified_gmt ? wc_string_to_timestamp( $post_object->post_modified_gmt ) : null,
+				'date_created'      => '0000-00-00 00:00:00' !== $post_object->post_date_gmt ? wc_string_to_timestamp( $post_object->post_date_gmt ) : null,
+				'date_modified'     => '0000-00-00 00:00:00' !== $post_object->post_modified_gmt ? wc_string_to_timestamp( $post_object->post_modified_gmt ) : null,
 				'status'            => $post_object->post_status,
 				'menu_order'        => $post_object->menu_order,
 				'reviews_allowed'   => 'open' === $post_object->comment_status,
